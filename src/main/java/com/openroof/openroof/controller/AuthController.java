890c14094889f78de78e7721e4ba27a8f6285b16
Enrique_UNI <enrique.rios2019@fiuni.edu.py>
2026-02-23 15:07:56 -0300

    Login implementado.

--- src/main/java/com/openroof/openroof/controller/AuthController.java
+++ src/main/java/com/openroof/openroof/controller/AuthController.java
@@ -9,18 +9,23 @@ import com.openroof.openroof.dto.security.AuthResponse;
 import com.openroof.openroof.dto.security.LoginRequest;
 import com.openroof.openroof.service.AuthService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 import org.springframework.web.bind.annotation.RequestBody;
 import lombok.RequiredArgsConstructor;
 
-@RequestMapping("/auth")
 @RestController
+@RequestMapping("/auth")
 @RequiredArgsConstructor
+@Tag(name = "Authentication", description = "Endpoints para registro y login")
 public class AuthController {
+    
     private final AuthService authService;
 
+    @Operation(summary = "Iniciar sesión", description = "Retorna un token JWT válido")
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
         return ResponseEntity.ok(authService.login(request));
     }
-
 }
