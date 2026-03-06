029491204e3fb1366a25f0f39451fb5acfd2d745
Manuel Ayala <cesarayala@fiuni.edu.py>
2026-03-06 14:56:35 -0300

    OR-105: Implement AgentSpecialty feature with controller, service, and DTOs

--- src/main/java/com/openroof/openroof/repository/AgentSpecialtyRepository.java
+++ src/main/java/com/openroof/openroof/repository/AgentSpecialtyRepository.java
@@ -4,6 +4,10 @@ import com.openroof.openroof.model.agent.AgentSpecialty;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AgentSpecialtyRepository extends JpaRepository<AgentSpecialty, Long> {
+	boolean existsByName(String name);
+	Optional<AgentSpecialty> findByName(String name);
 }
