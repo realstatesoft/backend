428412deb89edf0966c372520f78621a15f7deb3
Manuel Ayala <cesarayala@fiuni.edu.py>
2026-03-12 14:43:24 -0300

    fix: update LeadController and SecurityConfig to remove 'api' prefix from leads endpoint

--- src/main/java/com/openroof/openroof/controller/LeadController.java
+++ src/main/java/com/openroof/openroof/controller/LeadController.java
@@ -15,7 +15,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/leads")
+@RequestMapping("/leads")
 @RequiredArgsConstructor
 public class LeadController {
 
