02076b568834d21cc7fda4250b68fa43468d566b
Manuel Ayala <cesarayala@fiuni.edu.py>
2026-05-09 15:48:30 -0300

    feat: add tenant screening DTOs, mapper, and repository; include notes field in tenant screenings

---
+++ src/main/java/com/openroof/openroof/model/enums/PetPolicy.java
@@ -0,0 +1,7 @@
+package com.openroof.openroof.model.enums;
+
+public enum PetPolicy {
+    NOT_ALLOWED,
+    ALLOWED,
+    ALLOWED_WITH_DEPOSIT
+}
---
+++ src/main/java/com/openroof/openroof/model/enums/UtilityType.java
@@ -0,0 +1,9 @@
+package com.openroof.openroof.model.enums;
+
+public enum UtilityType {
+    WATER,
+    ELECTRICITY,
+    GAS,
+    INTERNET,
+    TRASH
+}
