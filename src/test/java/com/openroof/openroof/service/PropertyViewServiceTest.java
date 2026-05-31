5143c32f3276aa8bc910277dcb9cb772a24dfce5
CarlosM <carlosmartinez@fiuni.edu.py>
2026-05-31 16:43:09 -0300

    SonarQ Fixes

--- src/test/java/com/openroof/openroof/service/PropertyViewServiceTest.java
+++ src/test/java/com/openroof/openroof/service/PropertyViewServiceTest.java
@@ -64,11 +64,10 @@ class PropertyViewServiceTest {
         User user = user(1L);
         Property property = property(10L, "Casa 1");
         when(propertyRepository.findById(10L)).thenReturn(Optional.of(property));
-        when(propertyViewRepository.findFirstByUser_IdAndProperty_IdOrderByCreatedAtDesc(1L, 10L))
-                .thenReturn(Optional.empty());
 
         propertyViewService.registerRecentView(10L, user);
 
+        verify(propertyViewRepository).deleteByUserIdAndPropertyId(1L, 10L);
         verify(propertyViewRepository).save(argThat(view ->
                 view.getUser().getId().equals(1L) && view.getProperty().getId().equals(10L)));
     }
@@ -78,14 +77,11 @@ class PropertyViewServiceTest {
     void registerRecentView_replacesExistingView() {
         User user = user(1L);
         Property property = property(10L, "Casa 1");
-        PropertyView existing = PropertyView.builder().property(property).user(user).build();
         when(propertyRepository.findById(10L)).thenReturn(Optional.of(property));
-        when(propertyViewRepository.findFirstByUser_IdAndProperty_IdOrderByCreatedAtDesc(1L, 10L))
-                .thenReturn(Optional.of(existing));
 
         propertyViewService.registerRecentView(10L, user);
 
-        verify(propertyViewRepository).delete(existing);
+        verify(propertyViewRepository).deleteByUserIdAndPropertyId(1L, 10L);
         verify(propertyViewRepository).save(any(PropertyView.class));
     }
 
